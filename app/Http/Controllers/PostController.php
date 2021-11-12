af88eb4bcf2f5f93905cd340a700e303e87f6caa
matsu0017 <1210.smile-mi@i.softbank.jp>
2021-11-12 16:51:13 +0900

    [add]ページネーション

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,8 +9,6 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-    // ViewにController内で取得した変数を渡したい場合は、Viewインスタンスに対してwithというメソッドを使用する。
-    // この時、変数名 => 値という形で渡し、View側ではこの時指定した変数名で値を参照します。
-        return view('posts/index')->with(['posts' => $post->get()]);  
-    }
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
+    } 
 }
